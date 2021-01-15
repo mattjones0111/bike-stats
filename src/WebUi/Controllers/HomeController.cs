4f0ae85f648c65ac9edccf5008fcbfcb8b91b880
Matt Jones <github@jonesm.io>
2021-01-15 13:16:20 +0100

    no-tag remove cruft, add first test

--- src/WebUi/Controllers/HomeController.cs
+++ src/WebUi/Controllers/HomeController.cs
@@ -1,37 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using WebUi.Models;
 
 namespace WebUi.Controllers
 {
+    [Route("")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+        [HttpGet("error")]
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            ErrorViewModel response = new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+
+            return View(response);
         }
     }
 }
--- src/WebUi/Models/ErrorViewModel.cs
+++ src/WebUi/Models/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace WebUi.Models
 {
     public class ErrorViewModel
