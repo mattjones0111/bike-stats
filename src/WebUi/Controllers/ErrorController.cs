2b6bec2c99767af7ad65357c10ee49497de40400
Matt Jones <github@jonesm.io>
2021-01-17 11:38:37 +0100

    move error action

---
+++ src/WebUi/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿namespace WebUi.Controllers
+{
+    using System.Diagnostics;
+    using Microsoft.AspNetCore.Mvc;
+    using Models;
+
+    [Route("error")]
+    public class ErrorController : Controller
+    {
+        [HttpGet("")]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            ErrorViewModel response = new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+
+            return View(response);
+        }
+    }
+}
--- src/WebUi/Controllers/HomeController.cs
+++ src/WebUi/Controllers/HomeController.cs
@@ -1,11 +1,9 @@
 ﻿namespace WebUi.Controllers
 {
-    using System.Diagnostics;
     using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Rendering;
-    using Models;
     using Ports;
 
     [Route("")]
@@ -24,18 +22,6 @@
             await BindServiceLocations();
 
             return View();
-        }
-
-        [HttpGet("error")]
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            ErrorViewModel response = new ErrorViewModel
-            {
-                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
-            };
-
-            return View(response);
         }
 
         async Task BindServiceLocations()
